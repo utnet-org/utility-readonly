576ab2cb1d1e2af009fb033b1b35e7bd8cf4833e
jong-un-crypto <36259838+zwong91@users.noreply.github.com>
2024-01-16 18:32:38 +0800

    Add debug configuration for testrsa

--- runtime/runtime/src/actions.rs
+++ runtime/runtime/src/actions.rs
@@ -740,8 +740,19 @@ pub(crate) fn action_create_rsa2048_challenge(
         return Ok(());
     }
 
+    //FIXME： 计算发起challenge 的nonce 随机数
     // 直接使用 root证书里面的args, 如算力
-
+    let args = get_rsa2048_keys(state_update, &root_id, &challenge.public_key)?.unwrap().args;
+    match serde_json::from_slice::<serde_json::Value>(&args) {
+        Ok(parsed_args) => {
+            serde_json::to_string_pretty(&parsed_args)
+                .unwrap_or_else(|_| "".to_string())
+                .replace('\n', "\n                                 ")
+        }
+        Err(_) => {
+            return Ok(());
+        }
+    };
     return Ok(());
 }
 
